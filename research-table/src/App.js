12679df92f42fec7cb07e29ada4f81e80eec18f3
ocean-trace-research <traceoceanresearch@gmail.com>
2025-02-20 16:25:21 -0500

    Changed folder structure

---
+++ research-table/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExcelTable from './ExcelTable';
+
+function App() {
+
+  return (
+    <div className="App">
+        <ExcelTable />
+    </div>
+  );
+}
+
+export default App;
